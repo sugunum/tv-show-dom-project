7bcca23a11e8ff52792c022529eb07cc65ad8fdd
Fatma Oymak <sugunum@gmail.com>
2020-04-24 13:48:28 +0300

    tv-show-dom-project first attempt

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+
+  for (let i = 0; i < episodeList.length; i++) {
+
+    let placeHolder = document.createElement("div");
+    placeHolder.id = "placeHolder";
+    rootElem.appendChild(placeHolder); //(<div></div> created with loop without text) assigned the div inside the root
+
+  }
+
+
+
+
 }
 
 window.onload = setup;
